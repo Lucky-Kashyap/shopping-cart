f49f646bf69f65acbd96c83d3f21efb1623fbdb9
Lucky <luckykashyap3430@gmail.com>
2023-10-20 14:33:29 +0530

    apply context & reducer concept for state management

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,27 +1,32 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./ProductCard.css";
 
-export const ProductCard = ({ product,shopCount,setShopCount,setProduct }) => {
-  const { name, price, image,id } = product;
+export const ProductCard = ({
+  product,
+  shopCount,
+  setShopCount,
+  setProduct,
+}) => {
+  const { name, price, image, id } = product;
 
-  const passData=()=>{
-    setShopCount(shopCount+1);
+  // const passData=()=>{
+  //   setShopCount(shopCount+1);
 
-    // console.log(product);
-// setProduct(product);
+  // console.log(product);
+  // setProduct(product);
 
-    // return product;
-  }
+  // return product;
+  // }
 
   return (
     <div className="productCard">
-        <Link to={`/product/${id}`}>
-      <img src={image} alt={name} />
+      <Link to={`/product/${id}`}>
+        <img src={image} alt={name} />
       </Link>
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button onClick={passData}>Add To Cart</button>
+        <button>Add To Cart</button>
       </div>
     </div>
   );
