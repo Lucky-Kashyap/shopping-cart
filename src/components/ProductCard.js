841a030d4e13649f53c24b3a89e33a83861909bf
Lucky <luckykashyap3430@gmail.com>
2023-10-20 17:21:26 +0530

    facing issue while saving into localstorage

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,32 +5,40 @@ import { useEffect, useState } from "react";
 
 export const ProductCard = ({ product }) => {
   const [isInCart, setIsInCart] = useState(false);
-  const {cartList,addToCart,removeFromCart} = useCart();
+  const { cartList, addToCart, removeFromCart } = useCart();
   const { name, price, image, id } = product;
-  const save = JSON.parse(localStorage.getItem('cartList'));
+  // const {state,dispatch} = useCart();
+  // const save = JSON.parse(localStorage.getItem('cartList'));
   // const [save,setSave] = useState(JSON.parse(localStorage.getItem('product') || false));
   // console.log(product);
   // const { name, price, image, id } = JSON.parse(localStorage.getItem('product')) || "";
-// JSON.parse(localStorage.getItem('product'));
-
+  // JSON.parse(localStorage.getItem('product'));
 
   useEffect(() => {
-    localStorage.setItem('cartList',JSON.stringify(cartList));
-    const productIsInCart = cartList.find(cartItem => cartItem.id === id);
+    // const savedProd
+    // localStorage.setItem('cartList',JSON.stringify(cartList));
+    const productIsInCart = cartList.find((cartItem) => cartItem.id === id);
 
-    if(productIsInCart){
+    if (productIsInCart) {
       setIsInCart(true);
     } else {
       setIsInCart(false);
     }
 
     // localStorage.setItem('product',JSON.stringify(cartList));
-
-
-
-    
-
   }, [cartList, id]);
+
+  // useEffect(() => {
+  //   const savedProducts = JSON.parse(localStorage.getItem('cartList'));
+  //   if (savedProducts) {
+  //     dispatch({ type: 'LOAD_PRODUCTS', payload: savedProducts });
+  //   }
+  // }, []);
+
+  // Save products to localStorage whenever they change
+  // useEffect(() => {
+  //   localStorage.setItem('cartList', JSON.stringify(state.cartList));
+  // }, [state?.cartList]);
 
   // const passData=()=>{
   //   setShopCount(shopCount+1);
@@ -41,11 +49,11 @@ export const ProductCard = ({ product }) => {
   // return product;
   // }
 
-  const handleAdd = ()=>{
-    addToCart(product)
+  const handleAdd = () => {
+    addToCart(product);
     // console.log(product);
     // localStorage.setItem('product',JSON.stringify(product));
-  }
+  };
   return (
     <div className="productCard">
       <Link to={`/product/${id}`}>
@@ -55,8 +63,13 @@ export const ProductCard = ({ product }) => {
       <div className="action">
         <p>${price}</p>
         {/* <button onClick={handleAdd}>Add To Cart</button> */}
-        { isInCart ? (<button className="remove" onClick={() => removeFromCart(product)}>Remove</button>) :  (<button onClick={handleAdd}>Add To Cart</button>) }
-
+        {isInCart ? (
+          <button className="remove" onClick={() => removeFromCart(product)}>
+            Remove
+          </button>
+        ) : (
+          <button onClick={handleAdd}>Add To Cart</button>
+        )}
       </div>
     </div>
   );
